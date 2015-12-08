bb4b38db37e10063ad4113d17dc09171ccdb8e41
Cully Larson <cully.larson@gmai.com>
2015-12-08 00:06:25 -0800

    Added a script time time the execution of narr at various sizes of N.

---
+++ src/time.js
@@ -0,0 +1,12 @@
+import narr from "./index.js"
+
+const numFactors = 10
+
+narr(numFactors).reduce((n) => {
+    console.time(`Create ${n}}`)
+    narr(n)
+    console.timeEnd(`Create ${n}}`)
+
+    return n*10
+}, 1)
+
