3007451a0ac60b01b8d5019e4bd081d6c2c91ca6
Cully Larson <cully.larson@gmai.com>
2015-12-08 00:17:09 -0800

    Removed some extra braces.

--- src/time.js
+++ src/time.js
@@ -3,9 +3,9 @@ import narr from "./index.js"
 const numFactors = 10
 
 narr(numFactors).reduce((n) => {
-    console.time(`Create ${n}}`)
+    console.time(`Create ${n}`)
     narr(n)
-    console.timeEnd(`Create ${n}}`)
+    console.timeEnd(`Create ${n}`)
 
     return n*10
 }, 1)
